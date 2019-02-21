0d415c3f7c353536d701303f89ba4ffca6357033
Felix MERDINGER <fmerding@e1r3p3.42.fr>
2019-02-21 17:55:15 +0100

    F:10min plus tard

--- src/hitbox.c
+++ src/hitbox.c
@@ -7,29 +7,37 @@ int		ft_hitbox(t_s *s, int key)
 	int		x;
 	int		y;
 
-	x = s->xplayer / SPACE;
+
 	y = s->yplayer / SPACE;
+	x = s->xplayer / SPACE;
 	// printf("\033[33m%d > %d\033[0m\n", s->xplayer - 8, x * SPACE);
 
 	if (key == SDL_SCANCODE_A)
 	{
+		x = (s->xplayer - 9 )/ SPACE;
 		printf("\033[33m%d > %d\033[0m\n", s->xplayer - 8, x * SPACE);
 		if ((s->xplayer - 8) > (x * SPACE) && s->map[x][y]->envi > 999)
 			return (0);
 	}
 	if (key == SDL_SCANCODE_D)
 	{
+		x = (s->xplayer + 9 )/ SPACE;
 		printf("\033[33m%d < %d\033[0m\n", s->xplayer + 8, (x+1) * SPACE);
-		if ((s->xplayer + 8) < ((x + 1) * SPACE) && s->map[x][y]->envi > 999)
+		if ((s->xplayer + 8) < (x * SPACE) && s->map[x][y]->envi > 999)
 			return (0);
 	}
 	if (key == SDL_SCANCODE_W)
+	{
+		y = (s->yplayer - 9 )/ SPACE;
 		if ((s->yplayer - 8) > (y * SPACE) && s->map[x][y]->envi > 999)
 			return (0);
+	}
 	if (key == SDL_SCANCODE_S)
+	{
+		y = (s->yplayer + 9 )/ SPACE;
 		if ((s->yplayer + 8) < ((y + 1) * SPACE) && s->map[x][y]->envi > 999)
 			return (0);
-
+	}
 	printf("\033[32mYOU CAN MOVE\033[0m\n");
 	return (1);
 }
