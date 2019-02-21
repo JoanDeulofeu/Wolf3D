4e7c377c815b798d8ebaa578c891de6c48d37a37
Felix MERDINGER <fmerding@e1r3p3.42.fr>
2019-02-21 17:16:24 +0100

    F: coucou

--- src/display.c
+++ src/display.c
@@ -22,17 +22,98 @@ void	ft_display(t_s *s)
 						break;
 					}
 					if (event.key.keysym.scancode == SDL_SCANCODE_W)
-						s->posplayer.y -= 2;
+						// if (ft_hitbox(s, SDL_SCANCODE_W))
+							ft_dir_player(s, 1);// y-
 					if (event.key.keysym.scancode == SDL_SCANCODE_S)
-						s->posplayer.y += 2;
-					if (event.key.keysym.scancode == SDL_SCANCODE_A)
-						s->posplayer.x -= 2;
+						// if (ft_hitbox(s, SDL_SCANCODE_S))
+							ft_dir_player(s, 2);// y+
 					if (event.key.keysym.scancode == SDL_SCANCODE_D)
-						s->posplayer.x += 2;
-					printf("pos_pika_x=%d, pos_pika_y=%d\nxplay=%d, yplay=%d\nhigh_play=%d, width_play=%d\n\n", s->posplayer.x, s->posplayer.y, s->xplayer, s->yplayer, s->posplayer.h, s->posplayer.w);
+						// if (ft_hitbox(s, SDL_SCANCODE_D))
+							ft_dir_player(s, 3);// x+
+					if (event.key.keysym.scancode == SDL_SCANCODE_A)
+						// if (ft_hitbox(s, SDL_SCANCODE_A))
+							ft_dir_player(s, 4);// x-
+					if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+					{
+						s->dirplayer += 10;
+						if (s->dirplayer >= 400)
+							s->dirplayer = 0;
+					}
+					if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+					{
+						s->dirplayer -= 10;
+						if (s->dirplayer < 0)
+							s->dirplayer = 390;
+					}
+					// printf("pos_pika_x=%d, pos_pika_y=%d\nxplay=%d, yplay=%d\nhigh_play=%d, width_play=%d\n\n", s->posplayer.x, s->posplayer.y, s->xplayer, s->yplayer, s->posplayer.h, s->posplayer.w);
+					printf("pos_pika_x=%d, pos_pika_y=%d\nxplay=%d \ndirplayer=%f\nfloatx=%f\nfloaty=%f\n", s->posplayer.x, s->posplayer.y, s->xplayer, s->dirplayer, s->floatx, s->floaty);
 					ft_draw_minimap(s);
 				}
 			}
 		}
 	}
+}
+
+void ft_dir_player(t_s *s, int i)
+{
+	int tmp;
+	tmp = s->dirplayer;
+	if (i == 2)
+	{
+		if (s->dirplayer < 200)
+			s->dirplayer += 200;
+		else
+			s->dirplayer -= 200;
+	}
+	if (i == 4)
+	{
+		if (s->dirplayer < 100)
+			s->dirplayer += 300;
+		else
+			s->dirplayer -= 100;
+	}
+	if (i == 3)
+	{
+		if (s->dirplayer > 300)
+			s->dirplayer -= 100;
+		else
+			s->dirplayer += 100;
+	}
+	if (s->dirplayer <= 100)
+	{
+		s->floaty -= (1-(s->dirplayer/100));
+		if (s->floaty < s->posplayer.y - 0.5)
+			s->posplayer.y --;
+		s->floatx += (s->dirplayer/100);
+		if (s->floatx > s->posplayer.x + 0.5)
+			s->posplayer.x ++;
+	}
+	if (s->dirplayer > 100 && s->dirplayer <= 200)
+	{
+		s->floaty += ((s->dirplayer-100)/100);
+		if (s->floaty > s->posplayer.y + 0.5)
+			s->posplayer.y ++;
+		s->floatx += (1-((s->dirplayer-100)/100));
+		if (s->floatx > s->posplayer.x + 0.5)
+			s->posplayer.x ++;
+	}
+	if (s->dirplayer > 200 && s->dirplayer <= 300)
+	{
+		s->floaty += (1-((s->dirplayer-200)/100));
+		if (s->floaty > s->posplayer.y + 0.5)
+			s->posplayer.y ++;
+		s->floatx -= ((s->dirplayer-200)/100);
+		if (s->floatx < s->posplayer.x - 0.5)
+			s->posplayer.x --;
+	}
+	if (s->dirplayer > 300 && s->dirplayer <= 400)
+	{
+		s->floaty -= ((s->dirplayer-300)/100);
+		if (s->floaty < s->posplayer.y - 0.5)
+			s->posplayer.y --;
+		s->floatx -= (1-((s->dirplayer-300)/100));
+		if (s->floatx < s->posplayer.x - 0.5)
+			s->posplayer.x --;
+	}
+	s->dirplayer = tmp;
 }
